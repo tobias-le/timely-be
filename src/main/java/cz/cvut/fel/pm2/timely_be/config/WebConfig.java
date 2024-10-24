7f031a74092a6d4babeb1d313bab7efa8febfd4b
Tobias Le <letobias@fel.cvut.cz>
2024-10-24 22:28:00 +0200

    added cors allowed origins

---
+++ src/main/java/cz/cvut/fel/pm2/timely_be/config/WebConfig.java
@@ -0,0 +1,29 @@
+package cz.cvut.fel.pm2.timely_be.config;
+
+import lombok.Getter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@Getter
+@Configuration
+@ConfigurationProperties(prefix = "cors")
+public class WebConfig implements WebMvcConfigurer {
+    private List<String> allowedOrigins;
+
+    public void setAllowedOrigins(List<String> allowedOrigins) {
+        this.allowedOrigins = allowedOrigins;
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(allowedOrigins.toArray(new String[0]))
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
